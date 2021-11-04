f6b265aa7eab111ede10efde310964f1c6c23712
elineri <elin.ericstam@gmail.com>
2021-11-04 13:40:07 +0100

    Skapat constructors för Plant med dafaultvärden.

---
+++ Zoo/Plant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Plant : Organism
+    {
+        public bool _hasFlowers;
+
+        public Plant() : this("No rank given", false){ }
+
+        public Plant(string Rank, bool HasFlowers) : base (Rank)
+        {
+            this._hasFlowers = HasFlowers;
+        }
+    }
+}
