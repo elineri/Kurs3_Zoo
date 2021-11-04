ffc465eac8cae4f5d533b488ea17704381ea94d7
elineri <elin.ericstam@gmail.com>
2021-11-04 13:51:53 +0100

    Skapat unika egenskaper för Mammal och Reptile med constructorer.

--- Zoo/Mammal.cs
+++ Zoo/Mammal.cs
@@ -6,5 +6,13 @@ namespace Zoo
 {
     class Mammal : Animal
     {
+        public bool _isWild;
+
+        public Mammal() : this("Ingen rank angivet", "Inget namn angivet", "Inget kön angivet", 0, "Ingen föda angivet", "Inget levnadsområde angivet", true) { }
+
+        public Mammal(string Rank, string SciName, string Gender, int Age, string Diet, string LivingArea, bool IsWild) : base (Rank, SciName, Gender, Age, Diet, LivingArea)
+        {
+            this._isWild = IsWild;
+        }
     }
 }
--- Zoo/Reptile.cs
+++ Zoo/Reptile.cs
@@ -6,5 +6,12 @@ namespace Zoo
 {
     class Reptile : Animal
     {
+        public bool _hasLegs;
+        public Reptile() : this("Ingen rank angivet", "Inget namn angivet", "Inget kön angivet", 0, "Ingen föda angivet", "Inget levnadsområde angivet", true) { }
+
+        public Reptile(string Rank, string SciName, string Gender, int Age, string Diet, string LivingArea, bool HasLegs) : base(Rank, SciName, Gender, Age, Diet, LivingArea)
+        {
+            this._hasLegs = HasLegs;
+        }
     }
 }
